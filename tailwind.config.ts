47f88a807e6070d0982f59879e3cc97b528211f6
Daniel Steman <daniel.steman@bridgefund.nl>
2024-04-01 13:55:41 +0200

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
     colors: ({ colors }) => ({
