2037fa3c49a211cfbea15b3c068359f152d68090
Daniel Steman <daniel-steman@live.nl>
2024-04-28 14:17:20 +0200

    change font

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,10 @@ const Login = async () => {
   return (
     <div className="flex h-screen">
       <div className="m-auto text-center bg-stone-900 p-8 rounded-lg">
-        <h3 className="text-xl mb-8">Login to your account</h3>
+        <h3 className="text-xl mb-8 font-bold">Login to your account</h3>
         <button
           type="button"
-          className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2"
+          className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-md px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30  mb-2"
         >
           <BsSpotify color="#1DB954" fontSize={20} />
           &nbsp;&nbsp;Sign in with Spotify
