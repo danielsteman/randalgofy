0df19319b208b0c638d93d15cd39e25e362f3a56
Daniel Steman <daniel-steman@live.nl>
2024-04-28 10:59:40 +0200

    add login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,10 +3,16 @@ import { signIn } from "next-auth/react";
 
 const Login = async () => {
   return (
-    <div>
-      <button onClick={() => signIn("spotify", { callbackUrl: "/" })}>
-        Sign in with Spotify
-      </button>
+    <div className="flex h-screen">
+      <div className="m-auto text-center bg-stone-900 p-8 rounded-lg">
+        <h3 className="text-2xl mb-8">Login to your account</h3>
+        <button
+          className="bg-stone-800 text-white rounder-md py-2 px-4"
+          onClick={() => signIn("spotify", { callbackUrl: "/" })}
+        >
+          Sign in with Spotify
+        </button>
+      </div>
     </div>
   );
 };
